68ee3e96d8afe6b09895e00e3a9767d852c030ad
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-06-20 13:20:23 +0530

    code added

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 
+
 const jsonFilePath = process.argv[2];
+
+if (!jsonFilePath) {
+  console.error('Please provide the path to the JSON file as a command line argument.');
+  process.exit(1);
+}
+
 
 fs.readFile(jsonFilePath, 'utf8', (err, data) => {
   if (err) {
@@ -8,21 +15,25 @@ fs.readFile(jsonFilePath, 'utf8', (err, data) => {
     return;
   }
 
-  const users = JSON.parse(data);
+  let users;
+  try {
+    users = JSON.parse(data);
+  } catch (parseErr) {
+    console.error(`Error parsing JSON: ${parseErr}`);
+    return;
+  }
 
-  // TODO: Perform the required operations on the users data
-
-  // Print the total number of users
+ 
   console.log(`Total number of users: ${users.length}`);
 
-  // Find the user with the highest score and print their details
+  
   const highestScoreUser = users.reduce((prev, current) => (prev.score > current.score ? prev : current));
   console.log('User with the highest score:', highestScoreUser);
 
-  // Sort the users based on their scores in descending order
+ 
   users.sort((a, b) => b.score - a.score);
 
-  // Write the sorted data back to the JSON file
+  
   fs.writeFile(jsonFilePath, JSON.stringify(users, null, 2), (err) => {
     if (err) {
       console.error(`Error writing file: ${err}`);
